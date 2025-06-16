0a1cff3345c7bc7a2f3f922998c36a8a139767a2
Moksh Mahajan <125683965+mokshmahajan2004@users.noreply.github.com>
2025-06-16 19:04:12 +0530

    [75] [Time Beats: 100.00%] [Memory Beats: 78.95%] - LeetPush

---
+++ Sort-Colors.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) { //Most Optimal- Dutch National Flag Algorithm
+                                        //TC->O(N)  SC->O(1)
+        int low = 0, mid = 0, high = nums.size() - 1;
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                swap(nums[low], nums[mid]);
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                swap(nums[high], nums[mid]);
+                high--;
+            }
+        }
+    }
+};
