7a298a3c1023b22651ee67cb08959e03943e02dc
Moksh Mahajan <125683965+mokshmahajan2004@users.noreply.github.com>
2025-07-10 11:39:40 +0530

    [169] [Time Beats: 100.00%] [Memory Beats: 60.74%] - LeetPush

--- Majority-Element.cpp
+++ Majority-Element.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
-    //Overall TC->O(NlogN)+O(N)
-    //SC->O(N)  If every element is unique
-    int majorityElement(vector<int>& nums) {
-      unordered_map<int,int>mpp; //unordered map
-      //TC->N Log N (In case of unordered map, in best case and avg case this log N is removed and in worst case, it takes O(N) itself)
-      for(int i=0;i<nums.size();i++){
-        mpp[nums[i]]++;
-      }
-      //TC->O(N)
-        for(auto it:mpp){
-            if(it.second>(nums.size()/2)){
-                return it.first;
+    //Moore Voting Algorithm
+    //TC->O(N)+O(N)
+            //This is done to check the majority element, if it is stated that there is 
+            //always any majority element present then don't do this.
+    //OC->O(1)
+    int majorityElement(vector<int>& v) {
+        int cnt=0;
+        int el;
+        for(int i=0;i<v.size();i++){
+            if(cnt==0){
+                cnt=1;
+                el=v[i];
+            }
+            else if(v[i]==el){
+                cnt++;
+            }
+            else{
+                cnt--;
             }
         }
-     return -1;  
+        int cnt1=0;
+        for(int i=0;i<v.size();i++){
+            if(v[i]==el)cnt1++;
+        }
+        if(cnt1>(v.size()/2)){
+            return el;
+        }
+        return -1;
     }
 };
