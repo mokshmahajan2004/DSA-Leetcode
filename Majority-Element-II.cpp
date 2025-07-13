253fcd0d9c0f93124fb1ab652b2082a88a40fdfd
Moksh Mahajan <125683965+mokshmahajan2004@users.noreply.github.com>
2025-07-13 18:35:38 +0530

    [229] [Time Beats: 100.00%] [Memory Beats: 45.81%] - LeetPush

--- Majority-Element-II.cpp
+++ Majority-Element-II.cpp
@@ -1,23 +1,36 @@
 class Solution {
 public:
-    //TC->O(N*Logn)if ordered map
-    //TC->O(N*1)if unordered map
-    //for unordered map worst time complexity is O(N)
-    //SC->O(N)
+    //TC->O(2N)
+    //SC->O(1)
     vector<int> majorityElement(vector<int>& nums) {
-        vector<int>ls;
-        map<int,int>mpp;
-        int n=nums.size();
-        int mini=(int)(n/3)+1;
-        for(int i=0;i<n;i++){
-            mpp[nums[i]]++;
-            if(mpp[nums[i]]==mini){
-                ls.push_back(nums[i]);
-            }
-            if(ls.size()==2) break;
+        int cnt1=0,cnt2=0;
+        int el1=INT_MIN;
+        int el2=INT_MIN;
+        for(int i=0;i<nums.size();i++){
+           if(cnt1==0&&nums[i]!=el2){
+                cnt1=1;
+                el1=nums[i];
+           }
+           else if(cnt2==0&&nums[i]!=el1){
+                cnt2=1;
+                el2=nums[i];
+           }
+           else if(nums[i]==el1)cnt1++;
+           else if(nums[i]==el2)cnt2++;
+           else{
+            cnt1--;cnt2--;
+           }
         }
+        vector<int>ls;
+        cnt1=0,cnt2=0;
+        for(int i=0;i<nums.size();i++){
+            if(el1==nums[i])cnt1++;
+            if(el2==nums[i])cnt2++;
+        }
+        int mini=(int)(nums.size()/3)+1;
+        if(cnt1>=mini) ls.push_back(el1);
+        if(cnt2>=mini) ls.push_back(el2);
         sort(ls.begin(),ls.end());
         return ls;
-
     }
 };
