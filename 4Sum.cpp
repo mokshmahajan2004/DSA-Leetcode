e1c24fcb8190235628a206900ebf43fbbdd50ea3
Moksh Mahajan <125683965+mokshmahajan2004@users.noreply.github.com>
2025-07-16 19:32:53 +0530

    [18] [Time Beats: 81.90%] [Memory Beats: 58.16%] - LeetPush

--- 4Sum.cpp
+++ 4Sum.cpp
@@ -1,29 +1,36 @@
 class Solution {
 public:
-    // Better
-    // TC -> O(N^3 * log(m)), SC -> O(no of quads * 2)
+    //TC->O(N^2*N)
+    //SC->O(no. of quads) just to return the answer
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        int n = nums.size();
-        set<vector<int>> st;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                set<long long> hashset;
-                for (int k = j + 1; k < n; k++) {
-                    long long sum = 1LL * nums[i] + nums[j] + nums[k];
-                    long long fourth = 1LL * target - sum;
-
-                    if (hashset.find(fourth) != hashset.end()) {
-                        vector<int> temp = {nums[i], nums[j], nums[k], (int)fourth};
-                        sort(temp.begin(), temp.end());
-                        st.insert(temp);
+        int n=nums.size();
+        vector<vector<int>>ans;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n;i++){
+           if(i>0 && nums[i]==nums[i-1])continue;
+           for(int j=i+1;j<n;j++){
+               if(j!=i+1 && nums[j]==nums[j-1])continue;
+               int k=j+1;
+               int l=n-1;
+               while(k<l){
+                    long long sum=nums[i];
+                    sum+=nums[j];
+                    sum+=nums[k];
+                    sum+=nums[l];
+                    if(sum==target){
+                        vector<int>temp={nums[i],nums[j],nums[k],nums[l]};
+                        ans.push_back(temp);
+                        k++;l--;
+                        while(k<l && nums[k]==nums[k-1])k++;
+                        while(k<l && nums[l]==nums[l+1])l--;
                     }
-
-                    hashset.insert(nums[k]);
-                }
-            }
+                    else if(sum<target)k++;
+                    else l--;
+                } 
+           }
+           
         }
-
-        return vector<vector<int>>(st.begin(), st.end());
+    return ans; 
     }
+
 };
