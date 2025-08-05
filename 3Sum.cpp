6d23bb9e156c22f417c9ad53118ee5efb53ae5bb
Moksh Mahajan <125683965+mokshmahajan2004@users.noreply.github.com>
2025-08-05 13:08:28 +0530

    [15] [Time Beats: 35.74%] [Memory Beats: 21.84%] - LeetPush

---
+++ 3Sum.cpp
@@ -0,0 +1,34 @@
+class Solution {
+    //TC->O(Nlogn)+O(N^2)
+    //SC->O(N)
+public:
+    vector<vector<int>> threeSum(vector<int>& num) {
+        int N=num.size();
+        vector<vector<int>>ans;
+        sort(num.begin(),num.end());
+        for(int i=0;i<N;i++){
+            if(i>0 && num[i]==num[i-1])continue;
+            int j=i+1;
+            int k=N-1;
+            while(j<k){
+                int sum=num[i]+num[j]+num[k];
+                if(sum<0){
+                    j++;
+                }
+                else if(sum>0){
+                    k--;
+                }
+                else{
+                    vector<int>temp={num[i],num[j],num[k]};
+                    ans.push_back(temp);
+                    j++;
+                    k--;
+                    while(j<k && num[j]==num[j-1]) j++;
+                    while(j<k && num[k]==num[k+1]) k--;
+
+                }
+            }
+        }
+        return ans;
+    }
+};
