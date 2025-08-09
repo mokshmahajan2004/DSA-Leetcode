96e94b53cb92bbeb913f2fc9b38ada2414373230
Moksh Mahajan <125683965+mokshmahajan2004@users.noreply.github.com>
2025-08-09 19:17:47 +0530

    [39] [Time Beats: 100.00%] [Memory Beats: 72.81%] - LeetPush

---
+++ Combination-Sum.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+//Tc->O((2^t)*k)
+//SC->k*x {where x is number of combinations}
+    void findCombination(int ind,int target,vector<int>&arr,vector<vector<int>>&ans,vector<int>&ds){
+        if(ind==arr.size()){
+            if(target==0){
+                ans.push_back(ds);
+            }
+            return;
+        }
+        //pick the element
+        if(arr[ind]<=target){
+          ds.push_back(arr[ind]);
+          findCombination(ind,target-arr[ind],arr,ans,ds);
+          ds.pop_back();  
+        }
+
+        findCombination(ind+1,target,arr,ans,ds);
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>>ans;
+        vector<int>ds;
+        findCombination(0,target,candidates,ans,ds);
+        return ans;
+    }
+};
