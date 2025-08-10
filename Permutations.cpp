27ef607fbb98c3ac66afdd6ae36e5e7b78c7d8a4
Moksh Mahajan <125683965+mokshmahajan2004@users.noreply.github.com>
2025-08-10 22:30:15 +0530

    [46] [Time Beats: 100.00%] [Memory Beats: 67.98%] - LeetPush

---
+++ Permutations.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+//TC->O(n!*n)
+//SC->O(n!)
+//Auxilary Space->O(n)
+    void recurPermute(int index,vector<int>& nums, vector<vector<int>>&ans){
+        if(index==nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+        for(int i=index;i<nums.size();++i){
+            swap(nums[index],nums[i]);
+            recurPermute(index+1,nums,ans);
+            swap(nums[index],nums[i]); //backtracking
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>ans;
+        recurPermute(0,nums,ans);
+        return ans;
+    }
+};
