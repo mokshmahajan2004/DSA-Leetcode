37500b34b5468d46abe010b0d90aca9071085de0
Moksh Mahajan <125683965+mokshmahajan2004@users.noreply.github.com>
2025-08-25 15:09:26 +0530

    [53] [Time Beats: 19.51%] [Memory Beats: 52.18%] - LeetPush

--- Maximum-Subarray.cpp
+++ Maximum-Subarray.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
+//TC->O(N)
+//SC->O(1)
     int maxSubArray(vector<int>& nums) {
-        int sum = 0, maxi = INT_MIN;
-        for (int i = 0; i < nums.size(); i++) { //Kadane Algorithm 
-                                                //TC->O(N) SC->O(1)
-            sum += nums[i];
-            if (sum > maxi) {
-                maxi = sum;
+        int n =nums.size();
+        int maxi=INT_MIN;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+                if(sum>maxi){
+                    maxi=sum;
+                }
+            if(sum<0){
+                sum=0;
             }
-            if (sum < 0) {
-                sum = 0;
-            }
+            
         }
         return maxi;
     }
-    
-}; 
+};
